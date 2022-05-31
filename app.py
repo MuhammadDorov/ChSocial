8f3caea7ae3594eb2529306d540552522751cdfc
Muhammad <mn.dorov@northis.ru>
2022-05-31 15:35:29 +0300

    Added a Jinja2 template engine

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title = 'Главная страница')
 
 @app.errorhandler(404)
 def pageNoteFound(error):
-    return render_template('404.html')
+    return render_template('404.html', title = '404 ошибка')
 
 if __name__ == '__main__':
     app.run(debug = True)
